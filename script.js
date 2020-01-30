f6924b1d57af135ff5ecf3b3f29c34c64639fba6
JaceKace <jasonkitamura@gmail.com>
2020-01-30 16:36:00 -0500

    change to index

--- script.js
+++ script.js
@@ -10,19 +10,20 @@ var upperCase = document.querySelector('#button2');
 var numberCase = document.querySelector('#button3')
 var specialCase = document.querySelector('#button4');
 
+
 var finalString = "";
 var finalPass = "";
 
 //global variables made from inside infoPrompt()
-var passLength
-var passNumber
+var passLength;
+var passNumber;
 
 // Prompt function for character length of password
 infoPrompt();
 function infoPrompt () {
 
-passLength = prompt('How many characters would you like your password to be (8-128)') 
-passNumber = Number(passLength)
+passLength = prompt('How many characters would you like your password to be (8-128)?') ;
+passNumber = Number(passLength);
 
 // if character length is not 8-128, users receives alert and function restarts
 if (passNumber <8 || passNumber >128) {
@@ -41,10 +42,10 @@ if (isNaN(passNumber)) {
 function writePassword() {
 
     // creating array of characters for each check box
-var letter = "abcdefghijklmnopqrstuvwxyz";
-var letterU = letter.toUpperCase();
-var number = "1234567890";
-var special = "!@#$%^&*()";
+    var letter = "abcdefghijklmnopqrstuvwxyz";
+    var letterU = letter.toUpperCase();
+    var number = "1234567890";
+    var special = "!@#$%^&*()";
 
 // creating if statements for checked boxes. If none are checked, an alert pops and the function is exited
   if (letterCase.checked==false && upperCase.checked==false && numberCase.checked==false && specialCase.checked==false) {
@@ -68,9 +69,11 @@ var special = "!@#$%^&*()";
 
 // for loop to grab random character from finalString and add it to the final password (finalPass). Loop repeats for the number the user entered in the prompt at the start
   for (i=0; i < passNumber; i++) {
-    finalPass = finalPass+finalString[Math.floor(Math.random() * finalString.length)];
+    
+    finalPass =finalPass+finalString[Math.floor(Math.random() * finalString.length)];
  
   }
 //   pastes finalPass into password box
   passwordText.value = finalPass;
+
 }
